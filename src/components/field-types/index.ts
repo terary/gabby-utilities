05a0930c1d127b3a4a819993008341983452156e
Terary Chambers <terary@gmail.com>
2021-02-23 10:53:32 -0500

    Removed unused references

--- src/components/field-types/index.ts
+++ src/components/field-types/index.ts
@@ -5,6 +5,8 @@
 import { QInput } from './QInput';
 import { QNumber } from './QNumber';
 import { CONDITION_OPERATORS, UIValidatorError } from './IFieldType';
+
 export { CONDITION_OPERATORS, QInput, QNumber };
+
 export type { UIValidatorError };
 // fexport type UIValidatorError;
